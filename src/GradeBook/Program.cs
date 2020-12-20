418efc1d5a9743ccee3a8ef46d6b45a75e48252c
Roger Ng <roger.ng@3plearning.com>
2020-12-21 09:05:52 +1100

    init

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var grades = new List<double>() {12.5, 32.2, 44.9, 21.7};
+            grades.Add(55.4);
+            var result = 0.0;
+
+            foreach (var num in grades)
+            {
+                result += num;
+            }
+            result /= grades.Count;
+            Console.WriteLine(result:N1);
+        }
+    }
+}
